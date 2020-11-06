8ed8cf6ce27358cb06af2c5c63e489e5701c1117
eumsmo <juanriven10@gmail.com>
2020-11-06 16:59:58 -0300

    🔧 Fix no models.py
    Ajuste em algumas coisas do BD no models.py

--- serase_app/models.py
+++ serase_app/models.py
@@ -1,44 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-Class Usuario(models.model):
-    cod_user=models.CharField(max_length=10, primary_key= true )#AutoField(primary_key=True)
-    email= models.EmailField(max_length = 200) 
-    senha =models.CharField(max_length =30)
+class Saldo(models.model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    mes_ano = models.DateField()
+    saldo = models.DecimalField(decimal_places=2)
     
+class Movimentacao(models.model):
+    data = models.DateField()
+    valor_esperado = models.DecimalField(decimal_places=2)
+    data_geracao = models.DateField()
+    data_lancamento = models.DateField()
+    valor_pago = models.DecimalField(decimal_places=2)
+    usuario = cod_user=models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeingKey(Categoria, on_delete=models.CASCADE)
 
-Class Saldo(models.model)
-   
-    cod_user=models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    mes_ano=  models.IntegerField()#similar a periodo
-    saldo= models.DecimalField(decimal_places=2)
+class Categoria(models.model):
+    nome = models.CharField(max_length=20)
     
-    
-Class Movimentacao(models.model)
-    cod_movimentacao=models.AutoField(primary_key=True)
-    data =models.DateField()
-    valor_a _pagar= models.DecimalField(decimal_places=2)
-    dat_geracao= models.DateField
-    data_geracao=models.DateField
-    valor_pago= models.DecimalField(decimal_places=2)
-    cod_user=cod_user=models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    cod_cat =models.ForeingKey(Categoria,on_delete=models.CASCADE )
-
-Class Categoria(models.model)
-    cod_cat =models.AutoField(primary_key=True)
-    nome_cat =models.CharField(max_length=2)
-    
-Class PadraoMovimentacao(models.model)
-    cod_padrao=AutoField(primary_key=True)
-    receitaDespesa=models.BooleanField()
-    descricao=models.TextField()
-    periodo =  models.IntegerField()
-    valor=models.DecimalField(decimal_places=2)
-    dia_cobranca-models.DateField()
-    dat_inicio=models.DateField()
-    dat_fim= models.DateField()
-    cod_user=cod_user=models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    cod_cat =models.ForeingKey(Categoria,on_delete=models.CASCADE )
-
-    
-    
+class PadraoMovimentacao(models.model):
+    receita_despesa = models.BooleanField()
+    descricao = models.TextField()
+    periodo = models.IntegerField()
+    valor = models.DecimalField(decimal_places=2)
+    dia_cobranca = models.DateField()
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    usuario = cod_user=models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeingKey(Categoria, on_delete=models.CASCADE)
