9854f14b17ac0540726967d3e6bdd2e81434c53a
eumsmo <juanriven10@gmail.com>
2020-11-10 20:19:01 -0300

    👤 O ADM tá ON
    Modelos adicionados a interface de admin.
    Também adicionei minha conta de superusuário ao BD, recomendo que façam o mesmo.

--- serase_app/admin.py
+++ serase_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Saldo, Categoria, Movimentacao, PadraoMovimentacao
 
 # Register your models here.
+
+admin.site.register(Saldo)
+admin.site.register(Categoria)
+admin.site.register(Movimentacao)
+admin.site.register(PadraoMovimentacao)
