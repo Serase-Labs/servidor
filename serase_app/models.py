ee05d77a8bd19b4f45f7bf6d024e32672b0fea7e
eumsmo <juanriven10@gmail.com>
2020-11-19 11:05:39 -0300

    💅 Formatando o models.py
    Só pra ficar bonito mesmo :)

--- serase_app/models.py
+++ serase_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Saldo(models.Model):
     cod_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     mes_ano = models.DateField()
-    saldo = models.DecimalField(max_digits=10 ,decimal_places=2)
+    saldo = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=20)
@@ -14,29 +14,29 @@ class Categoria(models.Model):
         return self.nome
 
 class PadraoMovimentacao(models.Model):
-    tipo_movimentacao =(("Receita","Receita"),("Despesa","Despesa"))
-    receita_despesa = models.CharField(max_length=7, choices= tipo_movimentacao)
-    descricao = models.TextField(null=True,blank= True)
+    TIPO_MOVIMENTACAO = (("Receita","Receita"),("Despesa","Despesa"))
+    
+    receita_despesa = models.CharField(max_length=7, choices=TIPO_MOVIMENTACAO)
+    descricao = models.TextField(null=True, blank=True)
     periodo = models.IntegerField()
-    valor = models.DecimalField(max_digits=6 ,decimal_places=2, null = True, blank= True)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     dia_cobranca = models.IntegerField()
-    data_inicio = models.DateField(null=True,blank= True)
-    data_fim = models.DateField(null=True, blank= True)
+    data_inicio = models.DateField(null=True, blank=True)
+    data_fim = models.DateField(null=True, blank=True)
     cod_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     cod_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     def __str__(self):
         return self.descricao 
 
 class Movimentacao(models.Model):
-    
-    valor_esperado = models.DecimalField(max_digits=6 ,decimal_places=2,null=True, blank=True)
-    data_geracao = models.DateField(null=True,blank= True)
-    data_lancamento = models.DateField(null=True,blank= True)
-    valor_pago = models.DecimalField(max_digits=9 ,decimal_places=2,null=True,blank= True)
+    descricao = models.CharField(max_length=40, null=True, blank=True)
+    valor_esperado = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    valor_pago = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    data_geracao = models.DateField(null=True, blank=True)
+    data_lancamento = models.DateField(null=True, blank=True)
     cod_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     cod_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    descricao= models.CharField(max_length=40, null=True,blank= True)
-    cod_padrao = models.ForeignKey(PadraoMovimentacao, on_delete= models.CASCADE,blank=True , null=True)
+    cod_padrao = models.ForeignKey(PadraoMovimentacao, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
-        return   self.descricao 
+        return self.descricao 
 
