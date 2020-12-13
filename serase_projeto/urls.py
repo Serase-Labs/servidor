c31b8e666d23fdc03f0c9978cb319ceaafe478e1
jv_eumsmo <juanriven10@gmail.com>
2020-12-13 20:48:13 -0300

    🔧 FIX: problema no merge

--- serase_projeto/urls.py
+++ serase_projeto/urls.py
@@ -24,10 +24,7 @@ urlpatterns = [
     path('movimentacoes/', MovimentacaoSimplesView.as_view(), name='movimentacoes'),
     path('status/', StatusServidorView.as_view(), name='status'),
     path('saldo/', SaldoView.as_view(), name='saldo'),
-<<<<<<< HEAD
     path('usuario/', InformacoesUsuarioView.as_view(), name='usuario'),
-    path('relatorio/', include('serase_relatorio.urls')),
-=======
-    path('inserir_movimentacao',Insere_Mov.as_view(), name='insere_mov')
->>>>>>> insere-movimentacao
+    path('inserir_movimentacao',Insere_Mov.as_view(), name='insere_mov'),
+    path('relatorio/', include('serase_relatorio.urls')), # Inserir URLs antes dessa linha
 ]
